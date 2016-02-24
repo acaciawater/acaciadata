2d2695ffa33324991f3d505ab706f757ea2b58ef
theo <tkleinen@gmail.com>
2016-02-24 11:57:11 +0100

    fixes content types for series and charts (needed when using loaddata)

---
+++ acacia/data/migrations/0002_fix_contenttypes.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.contrib.contenttypes.models import ContentType
+
+def fixct(apps, schema_editor):
+    for name in ['Series', 'Chart']:
+        model = apps.get_model('data', name)
+        ct = ContentType.objects.get_for_model(model)
+        for f in model.objects.all():
+            f.polymorphic_ctype_id = ct.id
+            f.save()
+            
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(fixct),
+    ]
