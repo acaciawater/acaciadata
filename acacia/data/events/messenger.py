dbc17bc30930d950afd9e1597005897f33ecd26f
theo <tkleinen@gmail.com>
2016-03-07 20:32:53 +0100

    process_triggers removed, process_events added, sends all unset messages from history messages modified

--- acacia/data/events/messenger.py
+++ acacia/data/events/messenger.py
@@ -5,19 +5,21 @@ Created on Feb 20, 2016
 '''
 
 from django.conf import settings
+from .models import ACTION_EMAIL, ACTION_SMS
 
 class Messenger():
     ''' buffers triggered events and automatically sends messages to users through email or sms'''
 
     def __init__(self, event):
+        assert event, 'Event cannot be None'
         self.event = event
-        self.history = []
+        self.unsent = list(event.history_set.filter(sent=False))
         
     def __enter__(self):
         return self
 
     def __exit__(self, _type, _value, _traceback):
-        if self.history:
+        if self.unsent:
             self.send_messages()
  
     def send_sms(self, originator, body, recipients):
@@ -40,27 +42,77 @@ class Messenger():
         return m.send()
 
     def send_messages(self):
-        unsent = [h for h in self.history if h.sent == False]
         message = self.event.message
-        if self.event.action == 1:
-            if not message:
-                message ='\r\n'.join([h.format_html() for h in unsent])
-            success = self.send_email('Acaciadata Alarm', message, 'alarm@acaciadata.com', [self.event.target.email], html=True)
-        elif self.event.action == 2:
-            if not message:
-                message = 'Event {evt} was triggered {count} times'.format(evt=str(self.event.trigger), count=len(unsent))
-            success = self.send_sms('Acaciadata', message, self.event.target.cellphone)
+        try:
+            if self.event.action == ACTION_EMAIL:
+                if not message:
+                    message ='\r\n'.join([h.format_html() for h in self.unsent])
+                success = self.send_email('Acaciadata Alarm', message, 'alarm@acaciadata.com', [self.event.target.email], html=True)
+            elif self.event.action == ACTION_SMS:
+                if not message:
+                    message = 'Event {evt} was triggered {count} times'.format(evt=str(self.event.trigger), count=len(self.unsent))
+                success = self.send_sms('Acaciadata', message, self.event.target.cellphone)
+        except Exception as e:
+            logger.exception('Failed to send alarm message to {dest}: {ex}'.format(dest=unicode(self.event.target),ex=e))
+            success = False
         if success:
-            for h in unsent:
+            for h in self.unsent:
                 h.sent=True
                 h.save()
-
+            
     def add(self, message):
-        self.history.append(self.event.history_set.create(message=message, sent=False))
+        self.unsent.append(self.event.history_set.create(message=message, sent=False))
 
 import logging
 logger = logging.getLogger(__name__)
 
+def process_events(series):
+
+    # group events by trigger
+    from itertools import groupby
+    key = lambda e:e.trigger
+    events = sorted(series.event_set.all(),key=key)
+    eventgroups = groupby(events,key=key)
+ 
+    total = 0
+    for trigger,group in eventgroups:
+        # get last message sent for this trigger
+        start = None
+        events = list(group)
+        # remember last message sent
+        sent = {}
+        for e in events:
+            history = e.history_set.filter(sent=True).order_by('-date')
+            if history:
+                last = history[0].date
+                sent[e] = last
+                start = min(start, last) if start else last
+                
+        # select offending data points after last message sent
+        data = trigger.select(series,start=start)
+        num = data.count()
+        if num > 0:
+            total += num
+            # there are some alarms to be sent
+            for e in events:
+                last = sent.get(e,None)
+                with Messenger(e) as m:
+                    # add message for every event
+                    for date,value in data.iteritems():
+                        if last and date <= last:
+                            # dont send message twice
+                            continue
+                        m.add(e.format_message(date,value,html=True))
+                    msg = 'Event {name} was triggered {count} times for {ser} since {date}'.format(name=e.trigger.name, count=num, ser=series,date=start)
+        else:
+            msg = 'No alarms triggered for trigger {name} since {date}'.format(date=start, name=trigger.name)
+        logger.debug(msg)
+    if total > 0:
+        msg = '{name}: {num} alarms were triggered'.format(name=series.name, num=total)
+        logger.info(msg)
+    return total
+
+'''
 def process_triggers(series):
     # send messages to targets when events are triggered for time series
     total = 0
@@ -100,3 +152,4 @@ def process_triggers(series):
     if total > 0:
         msg = '{name}: {num} alarms were triggered'.format(name=series.name, num=total)
         logger.info(msg)
+'''
