bb6136b91eb9cacba8938ff60d0c3c7940049555
theo <tkleinen@gmail.com>
2016-10-18 10:21:07 +0200

    django > 1.8

--- acacia/urls.py
+++ acacia/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'acacia.views.home', name='home'),
+urlpatterns = [url(r'^$', 'acacia.views.home', name='home'),
     url(r'^cam/(?P<how>\w+)?$', 'acacia.views.cam', name='cam'),
     url(r'^mail$', 'acacia.views.mail'),
     url(r'^grappelli/', include('grappelli.urls')),
@@ -14,13 +13,11 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^data/', include('acacia.data.urls',namespace='acacia')),
-    #url(r'^spaarwater/', include('spaarwater.urls')),
     url(r'^spaarwater/', 'acacia.views.spaarwater'),
-)
+]
 
 from django.contrib.auth import views as auth_views
-urlpatterns += patterns('',
-    url(r'^password/change/$',
+urlpatterns += [url(r'^password/change/$',
                     auth_views.password_change,
                     name='password_change'),
     url(r'^password/change/done/$',
@@ -39,7 +36,7 @@ urlpatterns += patterns('',
                     auth_views.password_reset_confirm,
                     name='password_reset_confirm'),
     url(r'^accounts/', include('registration.backends.default.urls'))    
-)
+]
  
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
