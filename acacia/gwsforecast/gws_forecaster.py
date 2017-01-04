3fb9511d67e1b2606eef0c71be2d78fd4d758463
smoquet <sr_moquette@yahoo.com>
2017-01-04 09:11:34 +0100

    'gwsforecaster: adds forecasted rainfall as output'

--- acacia/gwsforecast/gws_forecaster.py
+++ acacia/gwsforecast/gws_forecaster.py
@@ -340,11 +340,12 @@ def gws_forecast(df_gws_hist, df_et_hist, df_pt_hist, df_et_mean_fc, df_pt_mean_
     df_voorspelling_h_max = pd.DataFrame(data=voorspelling_h_max, index = forecast_indices, columns = ['fc_gws_max'])
     
     df_h_day.columns = ["Grondwaterstand"]
-    
+    df_pt_day_voorsp.columns = ["apcpsfc_mean_from_today"]
     df_h_day_m = df_h_day[-300:]
     df_h_day_cm = df_h_day_m.multiply(100.0)
     join_1 = df_h_day_cm.join(df_voorspelling_h, how = 'outer')
     join_2 = join_1.join(df_voorspelling_h_min, how = 'outer')
+    join_2 = join_2.join(df_pt_day_voorsp[-12:], how = 'outer')
     joined_together = join_2.join(df_voorspelling_h_max, how = 'outer')    
     
     
