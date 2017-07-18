bfc9fcad662ef1011419c03afd499a0ff56cc596
theo <tkleinen@gmail.com>
2017-07-18 23:37:28 +0200

    migration merge

---
+++ acacia/data/migrations/0022_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data', '0021_auto_20170522_1433'),
+        ('data', '0018_auto_20161230_1559'),
+    ]
+
+    operations = [
+    ]
