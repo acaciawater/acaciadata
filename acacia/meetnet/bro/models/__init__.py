84719038826fb93f922185fed7cfb01f0c8266df
theo <tkleinen@gmail.com>
2019-11-28 13:15:41 +0100

    Empty sourcedocument model added

--- acacia/meetnet/bro/models/__init__.py
+++ acacia/meetnet/bro/models/__init__.py
@@ -1,5 +1,7 @@
+from .mapsheet import MapSheet
 from .code import Code
 from .codespace import CodeSpace
 from .groundwatermonitoringwell import GroundwaterMonitoringWell
 from .monitoringtube import MonitoringTube
 from .registrationrequest import RegistrationRequest
+from .sourcedocument import SourceDocument
---
+++ acacia/meetnet/bro/models/sourcedocument.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class SourceDocument(models.Model):
+    class Meta:
+        abstract = True
+        
