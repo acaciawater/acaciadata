fa6736dd458f7b6d3bc88aeb9118c47321b28f78
theo <tkleinen@gmail.com>
2019-11-28 13:14:26 +0100

    asxml code removed

--- acacia/meetnet/bro/models/registrationrequest.py
+++ acacia/meetnet/bro/models/registrationrequest.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from xml.etree.ElementTree import Element, SubElement
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from acacia.meetnet.bro.fields import CodeField
 from acacia.meetnet.bro.validators import ChamberOfCommerceValidator
-
 
 class RegistrationRequest(models.Model):
     requestReference = models.CharField(_('requestReference'),max_length=100)
@@ -16,17 +13,3 @@ class RegistrationRequest(models.Model):
     qualityRegime = CodeField(codeSpace='qualityRegime',verbose_name=_('Kwaliteitsregime'))
 #     broId = models.CharField(_('broid'),max_length=20)
     underPrivilege = models.NullBooleanField(_('Onder voorrecht'))
-    
-    def asxml(self):
-        ''' Creates xml with BRO request '''
-        request = Element('ns:registrationRequest',
-        {
-            'xmlns:ns':'http://www.broservices.nl/xsd/isgmw/1.1',
-            'xmlns:ns1':'http://www.broservices.nl/xsd/brocommon/3.0',        
-            'xmlns:ns2':'http://www.broservices.nl/xsd/gmwcommon/1.1',
-            'xmlns:ns3':'http://www.opengis.net/gml/3.2'
-        })
-        SubElement(request, 'ns1:requestReference').text = self.requestReference
-        SubElement(request, 'ns1:deliveryAccountableParty').text = self.deliveryAccountableParty
-        SubElement(request, 'ns1:qualityRegime').text = self.qualityRegime
-        return request
