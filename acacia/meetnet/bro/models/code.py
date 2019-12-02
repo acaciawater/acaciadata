c188aad3f3674c04f1ed8d046c1ec3960ae673b1
Theo Kleinendorst <tkleinen@gmail.com>
2019-12-02 16:42:03 +0100

    BRO typos and missing imports

--- acacia/meetnet/bro/models/code.py
+++ acacia/meetnet/bro/models/code.py
@@ -5,6 +5,7 @@ Created on Nov 27, 2019
 '''
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.utils.translation import ugettext_lazy as _
 
 from django.db import models
 from .codespace import CodeSpace
--- acacia/meetnet/bro/validators.py
+++ acacia/meetnet/bro/validators.py
@@ -1,4 +1,5 @@
 from django.core.validators import RegexValidator
+from django.utils.translation import ugettext_lazy as _
         
 ChamberOfCommerceValidator = RegexValidator(regex=r'\d{8}',message=_('Illegal chamber of commerce number'))
 
