fc0e1d57cb19b63848a67b7ed4ca4b430994c8e1
theo <tkleinen@gmail.com>
2019-12-04 16:18:12 +0100

    Adds BRO test case

--- acacia/meetnet/bro/tests.py
+++ acacia/meetnet/bro/tests.py
@@ -2,5 +2,24 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from acacia.meetnet.bro.models.registrationrequest import RegistrationRequest
 
 # Create your tests here.
+class TestRegister(TestCase):
+    fixtures = ['test_data']
+    
+    def setUp(self):
+        TestCase.setUp(self)
+        
+    def tearDown(self):
+        TestCase.tearDown(self)
+        
+    def test_xml(self):
+        ''' test creation of xml document '''
+        req = RegistrationRequest.objects.first()
+        xml = req.as_xml()
+        print(xml)
+        
+    def test_register(self):
+        ''' test actual registration '''
+        pass
