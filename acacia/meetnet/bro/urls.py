b9b3ffb599027fa0bf37c6bda6d5a30e9d4dab37
theo <tkleinen@gmail.com>
2019-12-17 15:35:48 +0100

    Use bro.Defaults for BRO registration requests

--- acacia/meetnet/bro/urls.py
+++ acacia/meetnet/bro/urls.py
@@ -4,8 +4,9 @@ Created on Nov 1, 2019
 @author: theo
 '''
 from django.conf.urls import url
-from acacia.meetnet.bro.views import download_gmw
+from acacia.meetnet.bro.views import download_gmw, DefaultsView
 
 urlpatterns = [
     url(r'^gmw/',download_gmw,name='gmw'),
+    url(r'^defaults/(?P<pk>\d+)/$',DefaultsView.as_view(),name='defaults'),
 ]
--- acacia/meetnet/bro/views.py
+++ acacia/meetnet/bro/views.py
@@ -7,6 +7,9 @@ from django.utils.text import slugify
 
 from acacia.meetnet.bro.gmw import registration_request
 from acacia.meetnet.models import Well
+from acacia.meetnet.bro.models import Defaults
+from django.views.generic import UpdateView
+from django.urls.base import reverse
 
 
 def download_gmw(request):
@@ -15,7 +18,7 @@ def download_gmw(request):
     zf = zipfile.ZipFile(io,'w')
     try:
         for well in Well.objects.all():
-            request = ElementTree(registration_request(well,kvk='73552739'))
+            request = ElementTree(registration_request(well))#,kvk='73552739'))
             xml = StringIO.StringIO()
             request.write(xml,xml_declaration=True,encoding='utf-8')
             zf.writestr(slugify(well.nitg or well.name) + '.xml', xml.getvalue())
@@ -25,3 +28,14 @@ def download_gmw(request):
     except Exception as e:
         resp = HttpResponseServerError(unicode(e))
     return resp
+
+class DefaultsView(UpdateView):
+    model = Defaults
+    fields = ('deliveryAccountableParty','owner','maintenanceResponsibleParty')
+    
+    def get_success_url(self):
+        return self.request.GET.get('next',reverse('bro:gmw'))
+    
+    def get_context_data(self, **kwargs):
+        return UpdateView.get_context_data(self, **kwargs)
+    
