de8b6c53a1050fdd32e2334f2529c79ce9063c2a
Theo Kleinendorst <tkleinen@gmail.com>
2020-02-09 23:24:53 +0100

    query for ellitrack loggers simplified

--- acacia/meetnet/management/commands/update_charts.py
+++ acacia/meetnet/management/commands/update_charts.py
@@ -4,11 +4,10 @@ Created on Apr 13, 2017
 @author: theo
 '''
 from django.core.management.base import BaseCommand
-from acacia.data.models import Datasource
 import logging
 from acacia.data.loggers import SourceAdapter
-from acacia.data.models import Generator
 from acacia.meetnet.actions import make_wellcharts
+from acacia.meetnet.models import Well
  
 class Command(BaseCommand):
     args = ''
@@ -20,21 +19,17 @@ class Command(BaseCommand):
                 type = int,
                 dest = 'pk',
                 default = None,
-                help = 'update single datasource')
+                help = 'update chart of single well')
 
     def handle(self, *args, **options):
         with SourceAdapter(logging.getLogger(__name__)) as logger:
             logger.source = ''
             logger.info('***UPDATE ELLITRACK CHARTS***')
-            elli = Generator.objects.get(name='Ellitrack')
             pk = options.get('pk', None)
-            if pk is None:
-                datasources = Datasource.objects.filter(generator=elli)
+            if pk:
+                wells = Well.objects.filter(pk=pk)
             else:
-                datasources = Datasource.objects.filter(pk=pk)
-            # find wells with Ellitrack logger
-            selected = [ds.loggerdatasource.logger for ds in datasources if hasattr(ds,'loggerdatasource')]
-            loggers = [lg for lg in selected if lg.model.startswith('etd')]
-            wells = [pos.screen.well for lgr in loggers for pos in lgr.loggerpos_set.all()]
+                # get all wells with ellitrack loggers
+                wells = Well.objects.filter(screen__loggerpos__logger__model__istartswith='etd').distinct()
             make_wellcharts(None,None,wells)
             logger.info('***UPDATE ELLITRACK CHARTS COMPLETED***')
