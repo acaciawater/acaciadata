4ab452cdf788468f864b46c0342e8d34f16a6391
Theo Kleinendorst <tkleinen@gmail.com>
2020-02-09 23:27:19 +0100

    typo

--- acacia/meetnet/actions.py
+++ acacia/meetnet/actions.py
@@ -111,7 +111,7 @@ update_statistics.short_description = 'statistiek vernieuwen'
 def update_sourcefiles(modeladmin, request, queryset):
     for lp in queryset:
         lp.update_files()
-update_statistics.short_description = 'set van bronbestanden bijwerken'
+update_sourcefiles.short_description = 'set van bronbestanden bijwerken'
 
 def make_wellcharts(modeladmin, request, queryset):
     for w in queryset:
