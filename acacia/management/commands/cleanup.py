648a9fa668babfdea9d56773f05447da10bcf7f8
theo <tkleinen@gmail.com>
2020-02-10 13:11:37 +0100

    cleanup regex

--- acacia/management/commands/cleanup.py
+++ acacia/management/commands/cleanup.py
@@ -4,7 +4,7 @@ Created on Feb 13, 2014
 @author: theo
 '''
 import os, re
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from acacia.data.models import Project, ProjectLocatie, MeetLocatie, Datasource, Parameter, Series
 from django.conf import settings
 
@@ -23,29 +23,33 @@ class Command(BaseCommand):
                 dest='dry',
                 default=False,
                 help="Dry run: don't delete the files")
+        parser.add_argument('-r','--regex',
+                dest='regex',
+                default=None,
+                help="Regular expression to filter filenames, e.g. 'knmi' ")
 
-    def process_folder(self,folder,inuse,dry,verbose=0):
+    def process_folder(self,folder,inuse,dry,pattern=None,verbose=0):
         count = 0
         bytes = 0
-	size = 0
-        for path, folders, files in os.walk(folder):
+        size = 0
+        for path, _folders, files in os.walk(folder):
             for f in files:
                 name = os.path.join(path,f)
-                if not name in inuse:
-                    if verbose:
-                        self.stdout.write('Deleting %s\n' % name)
-                    try:
-                        size = os.path.getsize(name)
-                        if not dry:
-                            os.remove(name)
-                        count = count+1
-                        bytes += size 
-                    except Exception as e:
-                        self.stdout.write('Error deleting %s: %s\n' % (name,e))
-                else:
-                    if dry and verbose:
-                        self.stdout.write('Keeping %s\n' % name)
-                    pass
+                if pattern is None or re.search(pattern, name): 
+                    if name not in inuse:  
+                        if dry or verbose:
+                            self.stdout.write('Deleting %s\n' % name)
+                        try:
+                            size = os.path.getsize(name)
+                            if not dry:
+                                os.remove(name)
+                            count = count+1
+                            bytes += size 
+                        except Exception as e:
+                            self.stdout.write('Error deleting %s: %s\n' % (name,e))
+                        continue
+                if verbose>1:
+                    self.stdout.write('Keeping %s\n' % name)
         return (count, bytes)
                 
     def handle(self, *args, **options):
@@ -76,9 +80,10 @@ class Command(BaseCommand):
         count = 0
         bytes = 0
         dry = options.get('dry')
+        regex = options.get('regex')
         verbosity = options.get('verbosity',0)
         for folder in roots:
-            c,b = self.process_folder(folder, inuse, dry, verbosity)
+            c,b = self.process_folder(folder, inuse, dry, regex, verbosity)
             count += c
             bytes += b
         if verbosity>0:
