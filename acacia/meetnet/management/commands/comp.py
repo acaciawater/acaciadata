1d66b0284b495bd96a4c41413b835e689f497871
Theo Kleinendorst <tkleinen@gmail.com>
2020-02-15 17:53:36 +0100

    adds --first option and sets mlocatie for new series

--- acacia/meetnet/management/commands/comp.py
+++ acacia/meetnet/management/commands/comp.py
@@ -15,21 +15,25 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-s', '--screen', type=str, dest='screen')
         parser.add_argument('-r', '--reset', action='store_true', default=False, dest='reset',help='Tijdreeksen opnieuw aanmaken (reset)')
+        parser.add_argument('-f', '--first', action='store', default=None, dest='first',help='Eerste put')
         
     def handle(self, *args, **options):
         user = User.objects.get(username='theo')
         screen_name = options.get('screen')
         reset = options.get('reset')
+        first = options.get('first')
         known_baros = {}
         wells = set()
-        queryset = Screen.objects.all()
+        queryset = Screen.objects.order_by('well__name', 'nr')
         if screen_name:
             well, nr = screen_name.split('/')
             queryset=queryset.filter(Q(well__name__iexact=well)|Q(well__nitg__iexact=well),nr=nr)
+        elif first:
+            queryset=queryset.filter(well__name__gte=first)
         for screen in queryset:
             logger.debug(unicode(screen))
             name = '%s COMP' % screen
-            series, created = Series.objects.get_or_create(name=name,defaults={'user':user})
+            series, created = screen.mloc.series_set.get_or_create(name=name,defaults={'user':user, 'timezone':'Etc/GMT-1'})
             start = None
             if created:
                 logger.debug('Created {}'.format(series))
