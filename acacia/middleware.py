def46ecd66d4c55d88f07298e0110ef76cdb4abe
theo <tkleinen@gmail.com>
2020-03-19 23:08:35 +0100

    adds 10.0.2.* to allowed hosts

--- acacia/middleware.py
+++ acacia/middleware.py
@@ -10,11 +10,12 @@ def FilterHostMiddleware(get_response):
         host = request.get_host().rsplit(':',1)[0] # remove port, if any
         if host.endswith('localhost'):
             allowed_hosts.add(host)
-        if host.startswith('192.168.1.'):
+        elif host.startswith('192.168.1.'):
             allowed_hosts.add(host)
-
+        elif host.startswith('10.0.2.'):
+            allowed_hosts.add(host)
         if host not in allowed_hosts:
-            raise HttpResponseForbidden
+            return HttpResponseForbidden('Host not allowed.')
 
         response = get_response(request)
         return response
