78457a67b24177ebaf00ab3a7f87cfb5d6e1fab8
theo <tkleinen@gmail.com>
2020-03-24 13:20:54 +0100

    read hostnames from settings

--- acacia/middleware.py
+++ acacia/middleware.py
@@ -1,23 +1,21 @@
 from django.http import HttpResponseForbidden
+from django.conf import settings
+from string import lower
 
 def FilterHostMiddleware(get_response):
     ''' 
         implements something like:
-        ALLOWED_HOSTS = ['.localhost', '192.168.1.*] 
+        ALLOWED_HOSTS = ['.localhost', '192.168.1.] 
     '''
     def middleware(request):
-        allowed_hosts = set(('127.0.0.1','localhost')) 
-        host = request.get_host().rsplit(':',1)[0] # remove port, if any
-        if host.endswith('localhost'):
-            allowed_hosts.add(host)
-        elif host.startswith('192.168.1.'):
-            allowed_hosts.add(host)
-        elif host.startswith('10.0.2.'):
-            allowed_hosts.add(host)
-        if host not in allowed_hosts:
-            return HttpResponseForbidden('Host not allowed.')
-
-        response = get_response(request)
-        return response
+        if hasattr(settings,'FILTER_HOSTS'):
+            allowed_hosts = map(lower, settings.FILTER_HOSTS)
+        else:
+            allowed_hosts = ('127.0.0.1','localhost') 
+        host = request.get_host().rsplit(':',1)[0].lower() # remove port, if any
+        for pattern in allowed_hosts:
+            if pattern in host:
+                return get_response(request)
+        return HttpResponseForbidden('Host not allowed.')
 
     return middleware
